c0941e560d96b42316029e2c0edcc274e54ec79f
Shreyashchimote <shreyashchimote08@gmail.com>
2026-03-28 01:01:12 +0530

    Container with most water

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -14,13 +14,29 @@ public class ContainerWithMostWater{
         System.out.print(Mostwater(height));
     }
     public static int Mostwater(ArrayList<Integer> height) {
-        int maxArea=0;
-        for(int i=0;i<height.size();i++){
-            for(int j=i+1; j<height.size();j++){
-                int width = j - i;
-                int h = Math.min(height.get(i), height.get(j));
-                int area = width * h;
-                maxArea=Math.max(maxArea, area);
+        // BRUTE FORCE APPROACHE
+        // int maxArea=0;
+        // for(int i=0;i<height.size();i++){
+        //     for(int j=i+1; j<height.size();j++){
+        //         int width = j - i;
+        //         int h = Math.min(height.get(i), height.get(j));
+        //         int area = width * h;
+        //         maxArea=Math.max(maxArea, area);
+        //     }
+        // }
+        // 2 pointer approach
+        int firstpointer=0;
+        int lastpointer=height.size()-1;
+         int maxArea=0;
+        while(firstpointer<lastpointer){
+            int width=lastpointer-firstpointer;
+            int ht=Math.min(height.get(firstpointer),height.get(lastpointer));
+            int area=width*ht;
+            maxArea=Math.max(maxArea, area);
+            if(height.get(firstpointer)<height.get(lastpointer)){
+                firstpointer++;
+            }else{
+                lastpointer--;
             }
         }
         return(maxArea);
