52f22cb825e12634175b6ec8a50f563db123c4f2
Shreyashchimote <shreyashchimote08@gmail.com>
2026-04-01 00:13:37 +0530

    Bit_Manipulation

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -34,6 +34,26 @@ public class Bit_Manipulation{
     }
     return n;
     }
+    public static int clrrangebits(int n, int i, int j) {
+        int a=((~0)<<(j+1));
+        int b=(1<<i)-1;
+        int bitmask=a|b;
+        return n&bitmask;
+    }
+    public static boolean ispowerof2(int n){
+        return (n & (n - 1)) == 0;
+    }
+    public static int numofsetbit(int n){
+        int bitmask=1;
+        int count=0;
+        while(n>0){
+            if((n&bitmask)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         // System.out.println(5&6);
         // System.out.println(5|6);
@@ -45,11 +65,14 @@ public class Bit_Manipulation{
         // isoddoreven(n);
         // isoddoreven(2);
         // isoddoreven(7);
-        getbit(10, 2);
-        System.out.println(setbit(10, 2));
-        System.out.println(clearbit(10, 1));
-        System.out.println(updatebit(10, 1, 0));
-        System.out.println(clearlastbits(15, 2));
+        // getbit(10, 2);
+        // System.out.println(setbit(10, 2));
+        // System.out.println(clearbit(10, 1));
+        // System.out.println(updatebit(10, 1, 0));
+        // System.out.println(clearlastbits(15, 2));
+        // System.out.println(clrrangebits(10, 2, 4));
+        // System.out.println(ispowerof2(6));
+        System.out.println(numofsetbit(15));
 
     }
 }
