57d02659ee0bbb276cfe68e0cb5d29c49dd3aa1e
Shreyashchimote <shreyashchimote08@gmail.com>
2026-04-26 11:45:11 +0530

    Cyclic list

--- CyclicLinkedList.java
+++ CyclicLinkedList.java
@@ -32,12 +32,39 @@ public class CyclicLinkedList{
         }
         return false;
     }
+    public static void removeCycle(){
+        Node slow=head;
+        Node fast=head;
+        boolean cycle=false;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        slow=head;
+        Node prev=null;
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        prev.next=null;
+    }
     public static void main(String[] args) {
         head=new Node(1);
         head.next=new Node(2);
+         Node temp=head.next;
         head.next.next=new Node(3);
         head.next.next.next=new Node(4);
-        // head.next.next.next.next=head;
+        head.next.next.next.next=temp;
+        System.out.println(iscycle());
+        removeCycle();
         System.out.println(iscycle());
     }
 }
